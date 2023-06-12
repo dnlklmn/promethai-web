69e332c1ffdd664dd930acfea7b019f9c570d829
dnlklmn <pelicansinberlin@gmail.com>
2023-06-12 22:18:28 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-between w-full px-16 py-3 text-white bg-primary">
+        <div>Logo</div>
+        <div>Menu</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
